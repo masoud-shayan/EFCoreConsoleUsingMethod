4e743544d9f875ef97bc187f5f9468be8d9180bd
masoud <masoud.shayannejad@gmail.com>
2020-02-14 21:07:03 +0330

    first step

---
+++ WorkingWithEFCore/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WorkingWithEFCore
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearsOfExperience  { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+    }
+}
---
+++ WorkingWithEFCore/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace WorkingWithEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // QueryingCategories(); 
+            // QueryingProducts();
+            // QueryingWithLike();
+            // AddProduct(6, "masoud shayan", 500M);
+            // IncreaseProductPrice("maso" , 10);
+            DeleteProducts("maso");
+        }
+
+
+        //select - where
+        static void QueryingCategories()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine(
+                    "Categories and how many products they have:"); // a query to get all categories and their related products
+                IQueryable<Category> cats = db.Categories
+                    .Include(c => c.Products); // does not need to use include(join) in lazy loading pattern
+                foreach (Category c in cats)
+                {
+                    WriteLine($"{c.CategoryName} has {c.Products.Count}products.");
+                }
+            }
+        }
+
+
+        //select - where
+        static void QueryingProducts()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("Products that cost more than a price, highest at top.");
+
+                string input;
+                decimal price;
+
+                do
+                {
+                    WriteLine("Enter a product price :");
+                    input = ReadLine();
+                } while (!decimal.TryParse(input, out price));
+
+                Console.WriteLine(price);
+
+                // server-side evaluating in EF core 3.1 but does not work on SQlite because unknown Decimal type
+
+                // IQueryable<Product> prods =  db.Products
+                //     .Where(product => product.Cost.GetValueOrDefault() > price)
+                //     .OrderByDescending(product => product.Cost);
+
+
+                // client-side evaluating in EF core 3.1 
+
+                // IEnumerable<Product> prods = db.Products
+                //     .AsEnumerable()
+                //     .Where(product => product.Cost > price)
+                //     .OrderByDescending(product => product.Cost)
+                //     .ToList();
+
+
+                // server-side evaluating in EF core 3.1 and works on SQlite because we convert decimal to double type
+
+                IQueryable<Product> prods = db.Products
+                    .Where(product => (double) product.Cost > (double) price)
+                    .OrderByDescending(product => (double) product.Cost);
+
+
+                foreach (var product in prods)
+                {
+                    WriteLine(
+                        $"pId : {product.ProductID} - pName : {product.ProductName} - pCost : {product.Cost:$#} - PUnit : {product.Stock} in Stock");
+                }
+            }
+        }
+
+
+        //select - where with like
+        static void QueryingWithLike()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("Enter part of a product name: ");
+                string input = ReadLine();
+
+                IQueryable<Product> prods = db.Products
+                    .Where(product => EF.Functions.Like(product.ProductName, $"%{input}%"));
+
+                foreach (var product in prods)
+                {
+                    WriteLine(
+                        $"name : {product.ProductName} - stock : {product.Stock} - discounted : {product.Discontinued}");
+                }
+            }
+        }
+
+
+        // select - orderByDescending 
+        static void ListProducts()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("{0,-3} {1,-35} {2,8} {3,5} {4}",
+                    "ID", "Product Name", "Cost", "Stock", "Disc.");
+
+                IQueryable<Product> prods = db.Products
+                    .OrderByDescending(p => (double) p.Cost);
+
+                foreach (var item in prods)
+                {
+                    WriteLine("{0:000} {1,-35} {2,8:$#,##0.00} {3,5} {4}",
+                        item.ProductID, item.ProductName, item.Cost,
+                        item.Stock, item.Discontinued);
+                }
+            }
+        }
+
+
+        // insert
+        static void AddProduct(int catergoryID, string productName, decimal? price)
+        {
+            using (var db = new Northwind())
+            {
+                var newProduct = new Product
+                {
+                    CategoryID = catergoryID,
+                    ProductName = productName,
+                    Cost = price
+                };
+
+                db.Products.Add(newProduct);
+
+                int affected = db.SaveChanges();
+
+
+                if (affected == 1)
+                {
+                    ListProducts();
+                }
+                else
+                {
+                    WriteLine("the last transaction did not execute");
+                }
+            }
+        }
+
+
+        // update
+        static void IncreaseProductPrice(string name, decimal amount)
+        {
+            using (var db = new Northwind())
+            {
+                // get first product whose name starts with name
+
+                Product updateProduct = db.Products
+                    .First(product => product.ProductName.StartsWith(name));
+
+                updateProduct.Cost += amount;
+
+                int affected = db.SaveChanges();
+
+                if (affected == 1)
+                {
+                    ListProducts();
+                }
+                else
+                {
+                    WriteLine("the last transaction did not execute");
+                }
+            }
+        }
+
+
+        //delete
+        static void DeleteProducts(string name)
+        {
+            using (var db = new Northwind())
+            {
+                IQueryable<Product> deleteProds = db.Products
+                    .Where(product => product.ProductName.StartsWith(name));
+
+                db.Products.RemoveRange(deleteProds);
+
+                int deleted = db.SaveChanges();
+                if (deleted > 0)
+                {
+                    WriteLine($"{deleted} product(s) were deleted.");
+                    ListProducts();
+                }
+                else
+                {
+                    WriteLine("the last transaction did not execute");
+                }
+            }
+        }
+    }
+}
